8e89d59a869187a883a6d6f309b13387f6b56600
Ryu-0824 <83778052+Ryuki-Arai@users.noreply.github.com>
2022-05-16 14:38:09 +0900

    Player Animation (4Directions)

--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -11,6 +11,9 @@ public class Player : MonoBehaviour
     [SerializeField] float _shootTime = 0.3f;
     [SerializeField] Bullet _prefab = null;
     [SerializeField] Transform _root = null;
+    Rigidbody2D _rb2d;
+    Animator _anim = default;
+    Vector2 verocity;
 
     float _timer = 0.0f;
 
@@ -23,16 +26,22 @@ public class Player : MonoBehaviour
 
     private void Start()
     {
+        _rb2d = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
         _bulletPool.SetBaseObj(_prefab, _root);
         _bulletPool.SetCapacity(100);
     }
 
     private void Update()
     {
-        float w = Input.GetAxis("Horizontal");
-        float h = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
 
-        transform.position += new Vector3(w * _speed * Time.deltaTime, h * _speed * Time.deltaTime, 0);
+        Vector2 dir = new Vector2(h, v);
+        verocity = dir.normalized * _speed;
+        _rb2d.velocity = verocity;
+
+        //transform.position += new Vector3(h * _speed * Time.deltaTime, v * _speed * Time.deltaTime, 0);
 
         _timer += Time.deltaTime;
         if (_timer > _shootTime)
@@ -41,6 +50,16 @@ public class Player : MonoBehaviour
             script.transform.position = this.transform.position;
             script.Shoot();
             _timer -= _shootTime;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (_anim)
+        {
+            Vector2 vector = _rb2d.velocity;
+            _anim.SetFloat("VectorX", vector.x);
+            _anim.SetFloat("VectorY", vector.y);
         }
     }
 }
